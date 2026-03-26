e6d6f52cfbe4b34fac54ec926f743ff250ff72a3
Akruti Rajiv Chopra <akruti.24bsa10197@vitbhopal.ac.in>
2026-03-26 15:42:34 +0530

    Update and rename script-3 to storage_permission_scan.sh

--- storage_permission_scan.sh
+++ storage_permission_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
-# Author: Akruti Chopra
+# Author: Akruti Rajiv Chopra
 # Audits important Linux directories and checks VLC config directory
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
